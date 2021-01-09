b3af22066b55884c8eba73c3afaf6ef20b650b56
k31tha <keith@kdasoft.co.uk>
2021-01-09 19:39:03 +0000

    remove sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    //"gatsby-plugin-sitemap",
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
