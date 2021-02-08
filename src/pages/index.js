6c487ce69ece15577bfdc1d6ca89036663c21c6b
k31tha <keith@kdasoft.co.uk>
2021-02-08 01:04:21 +0000

    added forgotten staged change

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const HomePage = () => {
     <Box as="section" marginBottom="1.45rem">
     <Container d="block" px="2rem" maxW="100%">
       <Heading as="h2">100 days of Gatsby</Heading>
-      <StaticImage src="../images/headphones-unsplash.jpg" alt="headphones" height={400} fit="scale"/>
+      <StaticImage src="../images/headphones-unsplash.jpg" alt="headphones" height={400} />
       </Container>
     </Box>
   )
